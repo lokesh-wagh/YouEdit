199f12f7cd82e0bb5c26e8c265d0e2132661bbf1
lokesh-wagh <lokeshwagh004@gmail.com>
2023-09-02 22:38:23 +0530

    plan for later

---
+++ app.js
@@ -0,0 +1,23 @@
+/* 
+    1. centralize all the 4 
+        for streaming a video make a endpoint redirect it to serve.js
+        for downloading make a endpoint redirect it
+        for uploading a youtube create a endpoint redirect it
+        No endpoint for tus tus would be directly communicated by frontend itself
+
+    2. integrate passport js , session to log the user's and editor's in
+    
+    3. design a schema for all the  view's how they are going to work
+        who will interact with what and so on
+    
+    4. code all the view's
+    
+    5. design the mongoose schema
+
+    6. implement findOrCreate logic in youtube for the token's to avoid unnecssary re-authorization
+
+    7. find a way to get google id along with the token's so that you know whose access you have
+
+    8. implement crud operation's on mongoose schema in appropriate place's
+
+*/
