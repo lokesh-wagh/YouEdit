657b44e393acce0d8e4c7300a216d447db284744
lokesh-wagh <lokeshwagh004@gmail.com>
2023-09-02 20:24:11 +0530

    plan for adding fileupload server side

---
+++ tus.js
@@ -0,0 +1,14 @@
+const {Server} = require('@tus/server') //get the server protocol mounter
+const {FileStore} = require('@tus/file-store') //get the storage area
+///thiss storage area can also be replaced by s3 and
+
+const host = '127.0.0.1' // select local host for server
+const port = 1080
+const server = new Server({
+  path: '/files', //give path
+  datastore: new FileStore({directory: './files'}), //return a directory that will give the values
+})
+
+
+
+server.listen({host, port}) //make the tus server listen for file upload's
