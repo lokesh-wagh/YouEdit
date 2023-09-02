deb80391818bca08a390989e0b28ef19a0f8cc2f
lokesh-wagh <lokeshwagh004@gmail.com>
2023-09-02 22:07:30 +0530

    all file's are set up

--- tus.js
+++ tus.js
@@ -1,5 +1,6 @@
-const {Server} = require('@tus/server') //get the server protocol mounter
+const {Server,EVENTS} = require('@tus/server') //get the server protocol mounter
 const {FileStore} = require('@tus/file-store') //get the storage area
+const fs=require('fs');
 ///thiss storage area can also be replaced by s3 and
 
 const host = '127.0.0.1' // select local host for server
@@ -9,7 +10,10 @@ const server = new Server({
   datastore: new FileStore({directory: './files'}), //return a directory that will give the values
   
 })
-
-//i plan to use the event listener's on server here to perform necessary process to the uploaded file's
+server.on(EVENTS.POST_FINISH,async(req,res,upload)=>{//EVENTS contains all the event's on this server
+  console.log(upload);
+  
+  await fs.rename(__dirname+'/files/'+upload.id,__dirname+'/files/'+upload.id+'.'+upload.metadata.filetype.split('/')[1],(e)=>{});
+})
 
 server.listen({host, port}) //make the tus server listen for file upload's
