fa2ccf1684867bccca7769638fa0c9ab1ca12443
lokesh-wagh <129964049+lokesh-wagh@users.noreply.github.com>
2024-05-29 15:47:33 +0530

    deploy1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './styledFrontend/App'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
-import SignIn from './styledFrontend/Auth.jsx'
+// import SignIn from './styledFrontend/Auth.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App/>
